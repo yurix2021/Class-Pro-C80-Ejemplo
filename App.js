0067653fe08c8f0a9220f402db5a0da5a33bab1b
yurix2021 <91973327+yurix2021@users.noreply.github.com>
2022-10-22 10:06:41 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const comidas=[4,7,7,3,1];
+comidas.sort();
+console.log(comidas);
+
+tomar2=comidas.slice(0,2);
+console.log(tomar2);
